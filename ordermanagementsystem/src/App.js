c991ac9e3b9e7d460989dec25a9e2c55b78b841a
yogeshluhar <work.yogeshluhar@gmail.com>
2025-06-13 20:17:27 +0530

    update itemcard..

--- ordermanagementsystem/src/App.js
+++ ordermanagementsystem/src/App.js
@@ -9,18 +9,12 @@ import HistoryCard from "./Order/historycard";
 import Customer from "./Component/Customer/customer";
 import Consumer from "./Component/Consumer/consumer";
 import Try, { ItemList } from "./ConsumerOrder/addtocard";
->>>>>>> 66224f9815b96ef8195daaa0e6fba1b0240f4f4a
 function App() {
 
   return (
     <div className="App">
       {/* <Header />
       <SwitchBtn /> */}
-<<<<<<< HEAD
-=======
-        <Consumer/>
-        {/* <Customer/> */}
->>>>>>> 66224f9815b96ef8195daaa0e6fba1b0240f4f4a
     </div>
   );
 }
