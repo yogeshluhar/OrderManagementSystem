134d10be0025c36d43383ebf1131b20f30f848b2
yogeshluhar <work.yogeshluhar@gmail.com>
2025-06-13 19:52:57 +0530

    vvv

--- ordermanagementsystem/src/App.js
+++ ordermanagementsystem/src/App.js
@@ -1,23 +1,13 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import Stylesheet from "reactjs-stylesheet";
 import "./App.css";
-import AddToProduct from "./Product/addToProduct";
-import CardItem from "./Product/cardItem";
-import ProductStatusButtons from "./Product/statusbtn";
-import BackgroundContainer from "./Reusable/Const/backgroundContainer";
-
 import Header from "./Reusable/Const/header";
 import SwitchBtn from "./Reusable/Const/switchbtn";
-import Ordercards from "./Order/ordercard";
-import HistoryCard from "./Order/historycard";
+
 function App() {
 
   return (
     <div className="App">
-      <Header />
-      <SwitchBtn />
-   
+      {/* <Header />
+      <SwitchBtn /> */}
     </div>
   );
 }
