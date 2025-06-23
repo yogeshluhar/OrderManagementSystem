1416b1cbb8df496d1d711537b108d5cc00bd28d5
roninstack101 <yashurathore2001@gmail.com>
2025-06-23 17:20:49 +0530

    Final Order Item

--- ordermanagementsystem/src/Reusable/Const/backgroundContainer.js
+++ ordermanagementsystem/src/Reusable/Const/backgroundContainer.js
@@ -1,8 +1,8 @@
 const styles = {
   bgContainer: {
     backgroundColor: "#d9e6ff",
+    // minHeight: "100%",
     height: '100vh',
-    // maxHeight: '100vh',
     margin: "10px 10px 0 10px",
     borderTopLeftRadius: "2rem",
     borderTopRightRadius: "2rem",
@@ -10,6 +10,7 @@ const styles = {
     justifyContent: "center", 
     padding: '10px',
     // overflowY: "auto", 
+    
   },
   // innerContainer: {
   //   width: "100%",
@@ -21,7 +22,7 @@ const styles = {
 
 const BackgroundContainer = ({ children }) => {
   return (
-    <div style={styles.bgContainer} className="hide-scrollbar">
+    <div style={styles.bgContainer} >
       {/* <div style={styles.innerContainer}>{children}</div> */}
       {children}
     </div>
